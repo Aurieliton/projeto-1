d5d15001f68611bea163f43ee535fa32bdd6f14e
Aurieliton <auriseya@gmail.com>
2020-01-24 11:13:01 -0300

    checkbox

--- lib/Forms/WidigetLogin.dart
+++ lib/Forms/WidigetLogin.dart
@@ -28,7 +28,7 @@ class _WidigetLoginState extends State<WidigetLogin> {
                 fontStyle: FontStyle.italic,
               ),
               decoration: InputDecoration(
-                labelText: 'Rogerio seinao !!!!] ',
+                labelText: 'Usuário',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(24.0)
                 )
